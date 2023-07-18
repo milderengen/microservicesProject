182a8376aa3ae456732c98b0739df467262ba5a8
milderengen <milosjanosik01@gmail.com>
2023-07-18 20:18:31 +0200

    initial commit

---
+++ src/main/java/com/example/microservicesproject/MicroservicesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesProjectApplication.class, args);
+	}
+
+}
